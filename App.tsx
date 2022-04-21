d0c3a9c70ec23123f921f5c2d54eadc976ea050a
Monica Rivera <moniriv@microsoft.com>
2022-04-20 21:30:07 -0700

    first commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import CalcScreen from './src/screens/CalcScreen'
+
+export default function App() {
+  return (
+    <View>
+      <CalcScreen />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ src/screens/CalcScreen.tsx
@@ -0,0 +1,34 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import CalcButton from '../components/CalcButton'
+
+export default function CalcScreen() {
+  return (
+    <View style={ styles.container}>
+        <View style={ styles.resultContainer }>
+      <Text style={ styles.resultText }>1,500.00</Text>
+      </View>
+      <CalcButton title ='1' />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create(
+    {
+        container: {
+            backgroundColor: 'black',
+            height: '100%',
+            padding: 10
+        },
+        resultContainer: {
+            flexDirection: 'row',
+            justifyContent: 'flex-end'
+
+        },
+        resultText:{
+            fontFamily: 'Arial',
+            fontSize: 60,
+            color: 'white'
+        }
+
+    })
