aadee0bad20b6bdf2e443e2e8b4b500c539dcacf
Monica Rivera <mriverap@outlook.com>
2022-04-20 23:01:37 -0700

    Apply eslint fixes

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import CalcScreen from './src/screens/CalcScreen'
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import CalcScreen from './src/screens/CalcScreen';
 
 export default function App() {
   return (
     <View>
       <CalcScreen />
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- src/components/CalcButton.tsx
+++ src/components/CalcButton.tsx
@@ -1,80 +1,75 @@
-import { StyleSheet, Text, View, TouchableOpacity, TouchableNativeFeedback, Platform } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  Platform,
+} from 'react-native';
+import React from 'react';
 
 interface Props {
-    title: string,
-    onPress?: () => void, //Function,
+  title: string;
+  onPress?: () => void; //Function,
 }
 
 const CalcButton = ({title, onPress}: Props) => {
   const ios = () => {
     return (
-        <TouchableOpacity
-        activeOpacity={ 0.8 }
-        style={[
-            styles.fabLocation 
-        ]}
-        onPress={onPress}
-        >
-          <View style={styles.fab}>
-                <Text style={styles.fabText}>{title}</Text>
-          </View>
-        </TouchableOpacity>
-      )
-
-  }
+      <TouchableOpacity
+        activeOpacity={0.8}
+        style={[styles.fabLocation]}
+        onPress={onPress}>
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>{title}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
 
   const android = () => {
     return (
-        <View
-        style={[
-            styles.fabLocation 
-        ]}
-        >
-        <TouchableNativeFeedback 
-        onPress={onPress}
-        background={TouchableNativeFeedback.Ripple('black', false, 30)}
-        >
+      <View style={[styles.fabLocation]}>
+        <TouchableNativeFeedback
+          onPress={onPress}
+          background={TouchableNativeFeedback.Ripple('black', false, 30)}>
           <View style={styles.fab}>
-                <Text style={styles.fabText}>{title}</Text>
+            <Text style={styles.fabText}>{title}</Text>
           </View>
         </TouchableNativeFeedback>
-        </View>
-      )
-  }
+      </View>
+    );
+  };
 
-  return (Platform.OS === 'ios') ? ios() : android();
-  
-}
+  return Platform.OS === 'ios' ? ios() : android();
+};
 
-export default CalcButton
+export default CalcButton;
 
 const styles = StyleSheet.create({
-    fab: {
-        backgroundColor: '#5856D6',
-        width: 60,
-        height: 60,
-        borderRadius: 100,
-        justifyContent: 'center',
-        shadowColor: '#000',
-        shadowOffset: {
-            width: 0,
-            height: 4,
-        },
-        shadowOpacity: 0.30,
-        shadowRadius: 4.65,
-        elevation: 8,
+  fab: {
+    backgroundColor: '#5856D6',
+    width: 60,
+    height: 60,
+    borderRadius: 100,
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 4,
     },
-    fabText: {
-        color: 'white',
-        fontSize: 30,
-        fontWeight: 'bold',
-        alignSelf: 'center'
-    },
-    fabLocation: {
-        position: 'absolute',
-        bottom: 25,
-    },
-
-
-})
+    shadowOpacity: 0.3,
+    shadowRadius: 4.65,
+    elevation: 8,
+  },
+  fabText: {
+    color: 'white',
+    fontSize: 30,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+  },
+  fabLocation: {
+    position: 'absolute',
+    bottom: 25,
+  },
+});
